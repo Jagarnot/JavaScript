de9f183ad8fb4d9f55c3feafcc502961df4a6e52
Jagarnot <jagarnot1236@gmail.com>
2023-01-19 09:49:18 -0300

    Alteração

--- Js 008.js
+++ Js 008.js
@@ -1,1 +1,6 @@
-console.log('Olá mundo')
+var vel = 55
+console.log(`A velocidade do seu carro é de ${vel} KM/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Se fudeo`)
+}
+console.log(`Dirija sempre com cuidado!!`) //Condição simples
---
+++ Js 009.js
@@ -0,0 +1,8 @@
+var pais = 'BRasil'
+console.log(`Vivendo no ${pais.toLowerCase()}`)
+if (pais == 'Brasil') {
+    console.log(`Seus pais é o ${pais.toLowerCase()}, Você é brasileiro`)
+}
+else {
+    console.log(`Seu pais é ${pais.toLowerCase()} Você é Estrangeiro`)
+}
