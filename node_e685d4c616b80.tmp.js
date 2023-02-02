e9bce8cf45878dce55ca0c934bb2f31be0b138d2
Jagarnot <jagarnot1236@gmail.com>
2023-02-02 19:20:12 -0300

    alteração

---
+++ node_e685d4c616b80.tmp.js
@@ -0,0 +1,19 @@
+var c = 1
+while (c <= 8) {
+    console.log('Tudo bem?')
+     // Este comando significa que o "c" recebe ele mesmo mais 1 está em começando em Js.
+}
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')*/
