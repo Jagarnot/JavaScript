a461741c990a5e22db1b44832f710733b8230940
Jagarnot <jagarnot1236@gmail.com>
2023-02-02 19:53:33 -0300

    alteração

--- Repetições.js
+++ Repetições.js
@@ -1,14 +1,16 @@
-var c = 1
+var c =1 
+do {
+    console.log(`Passo ${c}` )
+    c++
+} while(c <= 8)
+
+
+
+
+/*var c = 1
 while (c <= 8) {
-    console.log('Tudo bem?')
-    c++ // Este comando significa que o "c" recebe ele mesmo mais 1 está em começando em Js.
-}
-
-
-
-
-
-
+    console.log(`Passo ${c}`)
+    c++ // Este comando significa que o "c" recebe ele mesmo mais 1 está em começando em Js.}*/ 
 
 /*
 console.log('Tudo bem')
